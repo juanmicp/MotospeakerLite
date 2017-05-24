51460d25b5708e7aca035a4843714a5bdd97277b
Juanmi <Juanmicp@hotmail.com>
2017-05-24 08:31:48 +0200

    Escanea dispositivos bluetooth al alcance.

--- BluetoothManager.java
+++ BluetoothManager.java
@@ -2,10 +2,16 @@ package com.juanmi.motospeakerlite;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+
+import static android.R.attr.action;
 
 /**
  * Created by Juanmi on 22/05/2017.
@@ -13,29 +19,46 @@ import java.util.Set;
 
 public class BluetoothManager {
 
+    private List<BtDevice> btDevList = new ArrayList();
     private static BluetoothManager instance = null;
-    private BluetoothManager(){}
-    public static BluetoothManager getInstance(){
-        if(instance == null){
+
+    private BluetoothManager() {
+    }
+
+    public static BluetoothManager getInstance() {
+        if (instance == null) {
             instance = new BluetoothManager();
         }
         return instance;
     }
 
-    public List <BtDevice> scan(BluetoothAdapter btAdapter){
-        List <BtDevice> btDevList = new ArrayList();
-        btDevList.addAll(getAvailableDevices(btAdapter));
-        return btDevList;
+    public void scan(BluetoothAdapter btAdapter) {
+
+        if(btDevList != null)
+            btDevList.clear();
+
+        if(btAdapter.isDiscovering())
+            btAdapter.cancelDiscovery();
+
+        btAdapter.startDiscovery();
+
     }
 
-    public boolean connect (String devName){
+    public List<BtDevice> getBtDevList(){
+        return btDevList;
+    }
+    public void addToBtDevList(BtDevice device){
+        btDevList.add(device);
+    }
+
+    public boolean connect(String devName) {
         return false;
     }
 
-    private List <BtDevice> getAvailableDevices(BluetoothAdapter btAdapter){ //Devuelve los dispositivos asociados.
-        List <BtDevice> availableDevices = new ArrayList();
+    private List<BtDevice> getAvailableDevices(BluetoothAdapter btAdapter) { //Devuelve los dispositivos asociados.
+        List<BtDevice> availableDevices = new ArrayList();
         Set<BluetoothDevice> devs = btAdapter.getBondedDevices();
-        for (BluetoothDevice dev : devs){
+        for (BluetoothDevice dev : devs) {
             BtDevice device = new BtDevice(dev.getName(), dev.getAddress(), false);
             availableDevices.add(device);
         }
--- MainActivity.java
+++ MainActivity.java
@@ -1,8 +1,12 @@
 package com.juanmi.motospeakerlite;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,13 +17,17 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.nio.BufferOverflowException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.R.attr.action;
 
 public class MainActivity extends AppCompatActivity {
 
     private BluetoothAdapter btAdapter;
     private ListView devicesLV;
+    private List<BtDevice> btDevList = new ArrayList();
     private ArrayAdapter<BtDevice> adapter;
 
 
@@ -42,12 +50,20 @@ public class MainActivity extends AppCompatActivity {
             Toast t = Toast.makeText(getApplicationContext(), "Bluetooth habilitado.", Toast.LENGTH_SHORT);
             t.show();
         }
+        devicesLV = (ListView)findViewById(R.id.devicesLV);
+        adapter = new ArrayAdapter<BtDevice>(this, android.R.layout.simple_list_item_1, btDevList);
+        devicesLV.setAdapter(adapter);
 
     }
 
     public void scan(View view){ //Escanear y mostrar en lista los dispositivos bluetooth disponibles.
-        BluetoothManager btManager = BluetoothManager.getInstance();
-        List <BtDevice> btDevList = btManager.scan(btAdapter);
+        //BluetoothManager btManager = BluetoothManager.getInstance();
+        btAdapter.startDiscovery();
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(bReceiver, filter);
+        /*
+        //btManager.scan(btAdapter);
+        //List <BtDevice> btDevList = btManager.getBtDevList();
         devicesLV = (ListView) findViewById(R.id.devicesLV);
         adapter = new ArrayAdapter<BtDevice>(this, android.R.layout.simple_list_item_1, btDevList);
         devicesLV.setAdapter(adapter);
@@ -59,15 +75,42 @@ public class MainActivity extends AppCompatActivity {
                 connect(item);
             }
         });
+        */
+    }
+
+    public void makeVisible (View view){
+        if (btAdapter.isEnabled()){
+            Intent visibleBt = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            visibleBt.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 120);
+            startActivity(visibleBt);
+        }
+        else{
+            Toast.makeText(getBaseContext(), "No puebe hacerse visible. Bluetooth deshabilitado.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void connect(String devName){ //Conectar con el dispositivo seleccionado.
         BluetoothManager btManager = BluetoothManager.getInstance();
         if (btManager.connect(devName)){ //Si conecta con el dispositivo en cuestión.
-
+            /////////Abrir intent a ventana con cuadro dos cuadros de texto (envio y recepción).
         }
         else{
             Toast.makeText(getBaseContext(), "Imposible conectar con "+ devName+".", Toast.LENGTH_SHORT).show();
         }
     }
+
+    private final BroadcastReceiver bReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)){ //Cuando se detecta un nuevo dispositivo Bluetooth:
+                BluetoothDevice dev = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                BtDevice device = new BtDevice(dev.getName(), dev.getAddress(), false);
+                btDevList.add(device);
+                adapter.add(device);
+                //adapter.notifyDataSetChanged();
+                //devicesLV.setAdapter(new ArrayAdapter<BtDevice>(context, android.R.layout.simple_list_item_1, btDevList));
+            }
+        }
+    };
 }
